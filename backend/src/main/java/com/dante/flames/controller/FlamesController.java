b5097172b7ab241e1a814727c670d9f478823e94
aravind-jdev <aravind18l15@gmail.com>
2026-04-15 14:32:46 +0530

    CrossOrigins Updated

--- backend/src/main/java/com/dante/flames/controller/FlamesController.java
+++ backend/src/main/java/com/dante/flames/controller/FlamesController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://flamers.vercel.app")
+@CrossOrigin(origins = "https://flamers.aravindg.me")
 public class FlamesController {
 
     @Autowired
