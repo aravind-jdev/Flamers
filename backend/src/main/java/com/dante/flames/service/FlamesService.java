f130a9c6d31b16bc7a3d4b371b30ae8aea67c6f6
aravind-jdev <aravind18l15@gmail.com>
2026-04-19 19:41:37 +0530

    Service Update

--- backend/src/main/java/com/dante/flames/service/FlamesService.java
+++ backend/src/main/java/com/dante/flames/service/FlamesService.java
@@ -3,6 +3,9 @@ package com.dante.flames.service;
 import com.dante.flames.dto.RelationshipResponse;
 
 public interface FlamesService {
-RelationshipResponse getRelationship(String name1, String name2, String mode);
-
+    RelationshipResponse getRelationship(
+        String name1,
+        String name2,
+        String mode
+    );
 }
